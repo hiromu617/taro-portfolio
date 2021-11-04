9ee7c3eaf2fc85c38b8b5a1f486b978bf693fc1f
hiro <hiromhiro617@gmail.com>
2021-11-04 15:33:43 +0900

    :sparkles: add theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ChakraProvider, Container } from '@chakra-ui/react';
+import { theme } from '../theme/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Container bg="gray.400" p="5">
+        <Component {...pageProps} />
+      </Container>
+    </ChakraProvider>
+  );
 }
-export default MyApp
+export default MyApp;
